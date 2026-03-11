03bb30008e13748168a64976db36f6c8e9f0979a
Hasini Sai Reddy <kotlahasinisaireddy@gmail.com>
2026-03-11 19:34:01 +0530

    project v1.0

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
+import Home from './Home';
+
+function App() {
+ 
+  return (
+    <div>
+      <h1>SDP PRACTICE</h1>
+      <Home />
+     </div>
+  )
+}
+
+export default App
